b9f86ae3607c1629743c55a46d2f43050d20a26f
Casselen Angulo <casselenangulo@gmail.com>
2024-08-19 13:21:27 +0800

    localStorage added

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const saveBtn = document.getElementById("save-btn")
 let ulEl = document.getElementById("ul-el")
 let myLeads = []
 
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 saveBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    inputEl.value
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    inputEl.value = ""
     renderLeads()
 })
+
 function renderLeads(){
     let listItems = ""
     for(let i = 0; i < myLeads.length; i++){
